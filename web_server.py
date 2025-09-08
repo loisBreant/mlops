19f447e847eb5acc40d5afe1b4179e861d794109
lois.breant <lois.breant@epita.fr>
2025-09-08 14:42:39 +0200

    feat: init repo

--- web_server.py
+++ web_server.py
@@ -15,3 +15,9 @@ def predict(size: int, nb_rooms: int, garden: bool):
     prediction = model.predict([[size, nb_rooms, garden]])
     return {"y_pred": prediction[0]}
 
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=8051)
+
